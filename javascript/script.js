a9229c9c39fa6b1042ea95216abaea6024dd3fb4
Haseeb <haseebmurtaza21@gmail.com>
2020-12-10 07:31:53 -0800

    Changes to variable names

--- javascript/script.js
+++ javascript/script.js
@@ -3,13 +3,12 @@ var btn = document.querySelector("button").style.display = 'none';
 var input = document.querySelector("input").style.display = 'none';
 
 
-var inputs = document.querySelector(".container");
-    inputs.addEventListener("mouseover", hoverOver);
+var container = document.querySelector(".container");
+var a = container.addEventListener("mouseover", hoverOver)
+var displayed = document.querySelector(".displayed-img");
 
 function hoverOver() {
     document.querySelector("label").style.display = 'inline';
     document.querySelector("button").style.display = 'inline';
     document.querySelector("input").style.display = 'inline';
-
 }
-
